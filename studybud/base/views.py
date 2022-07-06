0a6bc53df4e858a4410914f5dafb30dff52995c2
Abhishek Mishra <abhiusedu@gmail.com>
2022-07-06 16:41:08 -0700

    Add main and navbar templates using django template engine.
    We also put a fake room data in base app and render that using the
    tempalte.

--- studybud/base/views.py
+++ studybud/base/views.py
@@ -1,10 +1,17 @@
-from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
 
+rooms = [
+    {'id': 1, 'name': 'Let\'s learn python!'},
+    {'id': 2, 'name': 'Design with me'},
+    {'id': 3, 'name': 'Frontend developers'},
+]
+
+
 def home(request):
-    return render(request, 'home.html')
+    context = {'rooms': rooms}
+    return render(request, 'home.html', context)
 
 def room(request):
     return render(request, 'room.html')
