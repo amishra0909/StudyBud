afc22c69e4369581ff4e6c872a262264c35697ed
Abhishek Mishra <abhiusedu@gmail.com>
2022-07-06 23:49:22 -0700

    Add search functionality
    Changed the view to show a topic column and filter by that query in
    views.

--- studybud/base/views.py
+++ studybud/base/views.py
@@ -1,6 +1,7 @@
 import re
 from django.shortcuts import render, redirect
-from .models import Room
+from django.db.models import Q
+from .models import Room, Topic
 from .forms import RoomForm
 # Create your views here.
 
@@ -12,8 +13,18 @@ from .forms import RoomForm
 
 
 def home(request):
-    rooms = Room.objects.all()
-    context = {'rooms': rooms}
+    q = request.GET.get('q')
+    query = q if q is not None else ''
+    rooms = Room.objects.filter(
+        Q(topic__name__icontains=query) |
+        Q(name__icontains=query) |
+        Q(description__icontains=query)
+        )
+
+    topics = Topic.objects.all()
+    room_count = rooms.count()
+
+    context = {'rooms': rooms, 'topics': topics, 'room_count': room_count}
     return render(request, 'base/home.html', context)
 
 def room(request, pk):
